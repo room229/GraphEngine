3a79afd3802487992ba50ac0ca821442121d041a
Gheorghica Radu <radugheorghe715@gmail.com>
2019-05-10 17:04:15 -0700

    Finished small menu.

--- samples/GraphGenerator/GraphGenerator/Program.cs
+++ samples/GraphGenerator/GraphGenerator/Program.cs
@@ -18,15 +18,27 @@ namespace GraphGenerator
             //GraphGen gg = new GraphGen(5, 5, 5);
             //gg.CreatGraph();
             //gg.Print_Nodes();
+
+
             Console.WriteLine("Choose engine command: ");
             Console.WriteLine("1. Create graph");
             Console.WriteLine("2. View created graph nodes\n");
             ConsoleKeyInfo keyb_Inp = Console.ReadKey();
             if (keyb_Inp.KeyChar == '1')
+            {
                 Console.WriteLine("\nNode count: ");
-                Console.WriteLine("Avg degree: ");
-                Console.WriteLine("Label count: ");
+                ConsoleKeyInfo key1 = Console.ReadKey();
 
+                Console.WriteLine("\nAvg degree: ");
+                ConsoleKeyInfo key2 = Console.ReadKey();
+
+                Console.WriteLine("\nLabel count: ");
+                ConsoleKeyInfo key3 = Console.ReadKey();
+                GraphGen gg = new GraphGen(key1.KeyChar - '0', key2.KeyChar - '0', key3.KeyChar - '0');
+                gg.CreatGraph();
+            }
+            else if (keyb_Inp.KeyChar == '2')
+                GraphGen.Print_Nodes();
 
 
 
